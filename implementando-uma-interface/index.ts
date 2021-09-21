dac688ea24e74ed47a90ff6f01f38566e28ce18a
Jota Nascimento <jotafn@icloud.com>
2021-09-21 10:57:22 -0300

    implementando-uma-interface

---
+++ implementando-uma-interface/index.ts
@@ -0,0 +1,83 @@
+interface UserProps{
+    firtsName:String
+    lastName:String
+    username:String
+    email:String
+    password:String
+    cpf:String
+    contacto:String
+    isAdmin:Boolean
+}
+class User implements UserProps {
+    firtsName!:String
+    lastName!:String
+    username!:String
+    email!:String
+    password!:String
+    cpf!:String
+    contacto!:String
+    isAdmin!:Boolean
+
+constructor(firtsName:String,lastName:String,username:String,email:String,password:String,cpf:String,contacto:String,isAdmin:Boolean){
+    this.firtsName = firtsName
+    this.lastName = lastName
+    this.username = username
+    this.email = email
+    this.password = password
+    this.cpf = cpf
+    this.contacto = contacto
+    this.isAdmin = isAdmin
+}
+
+getFirtsname(){
+    return this.firtsName;
+}
+setFirtsname(firtsName:string){
+    this.firtsName = firtsName;
+}
+getLastsname(){
+    return this.lastName;
+}
+setLastsname(lastName:string){
+    this.lastName = lastName;
+}
+getUserName(){
+    return this.username;
+}
+setUserName(username:string){
+    this.username = username;
+}
+getEmail(){
+    return this.email;
+}
+setEmail(email:string){
+    this.email = email;
+}
+getPassword(){
+    return this.password;
+}
+setPassword(password:string){
+    this.password = password;
+}
+getCpf(){
+    return this.cpf;
+}
+seCpf(cpf:string){
+    this.cpf = cpf;
+}
+getContacto(){
+    return this.contacto;
+}
+setContacto(contacto:string){
+    this.contacto = contacto;
+}
+getIsAdmin(){
+    return this.firtsName;
+}
+setIsAdmin(isAdmin:Boolean){
+    this.isAdmin = isAdmin;
+}
+
+}
+
+
--- type-scriptNode/index.ts
+++ type-scriptNode/index.ts
@@ -19,44 +19,76 @@
 // console.log(soma)
 
 // criando uma classe
-// class Pessoa{
-//     nome!:string
-//     idade!:number
-//     cpf!:string
+class Pessoa{
 
-//     constructor(nome:string,idade:number,cpf:string){
-//         this.nome = nome
-//         this.idade = idade // o primeiro idade vem do atributo da classe. O segundo vem do Contructor
-//         this.cpf = cpf
-//     }
-//     escreverPropriedades(){
-//         console.log(this.nome)
-//         console.log(this.idade)
-//         console.log(this.cpf)
-//     }
+    constructor(private nome:string,private idade:number,private cpf:string){
+        this.nome = nome
+        this.idade = idade // o primeiro idade vem do atributo da classe. O segundo vem do Contructor
+        this.cpf = cpf
+    }
+    escreverPropriedades(){
+        console.log(this.nome)
+        console.log(this.idade)
+        console.log(this.cpf)
+    }
 
+    getNome(){
+        return this.nome;
+    }
+
+    setNome(nome:string){
+        this.nome = nome;
+    }
+
+    getIdade(){
+        return this.idade;
+    }
+
+    setIdade(idade:number){
+        this.idade = idade;
+    }
+
+    getCpf(){
+        return this.cpf;
+    }
+
+    setCpf(cpf:string){
+        this.cpf = cpf;
+    }
+
+}
+
+let pessoa = new Pessoa("jota", 47, "111.111.111-11");
+
+console.log(pessoa.getIdade())
+
+
+
+// class Carro{
+//     marca!:string
+//     km!:number
+//     modelo!:string
+
+//     constructor(marca:string,km:number,modelo:string){
+//         this.marca = marca
+//         this.km = km
+//         this.modelo = modelo
+//     }
+//     passarCaracteristicas(){
+//         console.log(this.marca)
+//         console.log(this.km)
+//         console.log(this.modelo)
+//     }
+// }
+// let carro = new Carro("renault",13000,"duster");
+
+// // carro.passarCaracteristicas();
+
+// interface propriedadesCarros{
+//     marca: string
 // }
 
-// let pessoa = new Pessoa("jota", 47, "111.111.111-11");
+// class Carro implements propriedadesCarros{
+//     marca!:string
+// }
 
-// pessoa.escreverPropriedades();
-
-class Carro{
-    marca!:string
-    km!:number
-    modelo!:string
-
-    constructor(marca:string,km:number,modelo:string){
-        this.marca = marca
-        this.km = km
-        this.modelo = modelo
-    }
-    passarCaracteristicas(){
-        console.log(this.marca)
-        console.log(this.km)
-        console.log(this.modelo)
-    }
-}
-let carro = new Carro("renault",13000,"duster");
-
-carro.passarCaracteristicas();
