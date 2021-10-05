df17fa5d0b15efbe9b6cd9d003bb099571e8ee59
Jota Nascimento <jotafn@icloud.com>
2021-10-05 09:05:07 -0300

    Conversor de Moedas

---
+++ conversor-de-moeda/src/app/conversor/utils/modal-cotacao.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ConversorService } from '../services';
+
+import { ConversaoResponse, Conversao } from '../models';
+
+@Component({
+  selector: 'modal-cotacao',
+  templateUrl: './modal-cotacao.component.html',
+  styleUrls: ['./modal-cotacao.component.css']
+})
+export class ModalCotacaoComponent implements OnInit {
+
+  @Input() id: string;
+  @Input() conversaoResponse: ConversaoResponse;
+  @Input () conversao: Conversao = new Conversao();
+  @Output() onConfirm: EventEmitter<any> = new EventEmitter<any>;
+
+
+  constructor(private conversorService: ConversorService) { }
+
+  ngOnInit(): void {
+  }
+
+}
